9e6427d950e3d94d48c1a80726201ee370107c00
Zibran Zarif Amio <zibran.zarif.amio@gmail.com>
2024-08-28 16:31:36 +0600

    fe + leetcode update

--- leetcode/trees/diameter_binary_tree.py
+++ leetcode/trees/diameter_binary_tree.py
@@ -14,17 +14,19 @@ class Solution:
         diameter = float("-inf")
 
         def DFS(root):
+
+            nonlocal diameter
             
             if not root:
                 return 0
             
-            left_radius = 1 + DFS(root.left)
-            right_radius = 1 + DFS(root.right)
+            left_radius = DFS(root.left)
+            right_radius = DFS(root.right)
 
             diameter = max(diameter, left_radius + right_radius)
 
-            return max(left_radius, right_radius)
+            return 1 + max(left_radius, right_radius)
 
         DFS(root)
-        
+
         return diameter
